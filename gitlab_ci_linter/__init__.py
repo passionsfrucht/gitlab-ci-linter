0b159e517252ea187cde4fd8f8faac46473ef8e1
allburov <allburov@gmail.com>
2019-11-17 15:27:43 +0700

    Add --insecure flag

--- gitlab_ci_linter/__init__.py
+++ gitlab_ci_linter/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import ssl
 import sys
 import urllib.request
 
@@ -9,20 +10,22 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--server', default='https://gitlab.com')
     parser.add_argument('--filename', default='.gitlab-ci.yml')
+    parser.add_argument('-k', '--insecure', action='store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    return gitlab_ci_linter(args.server, args.filename)
+    return gitlab_ci_linter(args.server, args.filename, args.insecure)
 
 
-def gitlab_ci_linter(server, filename):
+def gitlab_ci_linter(server, filename, insecure):
     try:
         gitlab_ci_content = open(filename).read()
     except FileNotFoundError:
         print(f'File not found: {filename}', file=sys.stderr)
         return 1
+
 
     url = f'{server}/api/v4/ci/lint'
     content = {'content': gitlab_ci_content}
@@ -30,7 +33,10 @@ def gitlab_ci_linter(server, filename):
     r = urllib.request.Request(
         url, headers={'Content-Type': 'application/json'}, data=data
     )
-    with urllib.request.urlopen(r) as gitlab:
+
+    # Verify or not server certificate
+    ssl_ctx = ssl.SSLContext() if insecure else None
+    with urllib.request.urlopen(r, context=ssl_ctx) as gitlab:
         if gitlab.status not in range(200, 300):
             print(f'Server said {gitlab.status}: {gitlab.url}', file=sys.stderr)
             return 1
