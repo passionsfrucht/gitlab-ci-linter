72132fcbd2175ad4109c4711bf8b283862243dc4
allburov <allburov@gmail.com>
2020-06-15 17:26:27 +0700

    Use black format

--- gitlab_ci_linter/__init__.py
+++ gitlab_ci_linter/__init__.py
@@ -9,71 +9,73 @@ import urllib.request
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        '--server',
-        default='https://gitlab.com',
-        help='This server will check .gitlab-ci.yml',
-    )
-    parser.add_argument('--filename',
-                        default='.gitlab-ci.yml',
-                        help='Specify Gitlab CI filename')
-    parser.add_argument(
-        '-k',
-        '--insecure',
-        action='store_true',
-        help='Allow insecure server connections when using SSL',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        '--private-token',
-        help='Use this private token to authenticate on the server',
-        default=os.environ.get('GITLAB_PRIVATE_TOKEN'))
+        "--server",
+        default="https://gitlab.com",
+        help="This server will check .gitlab-ci.yml",
+    )
+    parser.add_argument(
+        "--filename", default=".gitlab-ci.yml", help="Specify Gitlab CI filename"
+    )
+    parser.add_argument(
+        "-k",
+        "--insecure",
+        action="store_true",
+        help="Allow insecure server connections when using SSL",
+    )
+    parser.add_argument(
+        "--private-token",
+        help="Use this private token to authenticate on the server",
+        default=os.environ.get("GITLAB_PRIVATE_TOKEN"),
+    )
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    return gitlab_ci_linter(args.server, args.filename, args.insecure,
-                            args.private_token)
+    return gitlab_ci_linter(
+        args.server, args.filename, args.insecure, args.private_token
+    )
 
 
 def gitlab_ci_linter(server, filename, insecure, private_token):
     try:
         gitlab_ci_content = open(filename).read()
     except FileNotFoundError:
-        print(f'File not found: {filename}', file=sys.stderr)
+        print(f"File not found: {filename}", file=sys.stderr)
         return 1
 
-    url = f'{server}/api/v4/ci/lint'
-    content = {'content': gitlab_ci_content}
-    data = json.dumps(content).encode('utf-8')
+    url = f"{server}/api/v4/ci/lint"
+    content = {"content": gitlab_ci_content}
+    data = json.dumps(content).encode("utf-8")
 
     r = urllib.request.Request(url, data=data)
 
-    r.add_header('Content-Type', 'application/json')
+    r.add_header("Content-Type", "application/json")
 
     if private_token:
-        r.add_header('PRIVATE-TOKEN', private_token)
+        r.add_header("PRIVATE-TOKEN", private_token)
 
     # Verify or not server certificate
     ssl_ctx = ssl.SSLContext() if insecure else None
     with urllib.request.urlopen(r, context=ssl_ctx) as gitlab:
         if gitlab.status not in range(200, 300):
-            print(f'Server said {gitlab.status}: {gitlab.url}',
-                  file=sys.stderr)
+            print(f"Server said {gitlab.status}: {gitlab.url}", file=sys.stderr)
             return 1
 
         response_raw = gitlab.read()
-        response = json.loads(response_raw.decode('utf-8'))
+        response = json.loads(response_raw.decode("utf-8"))
 
-    if response['status'] == 'valid':
-        print(f'{filename} is valid')
+    if response["status"] == "valid":
+        print(f"{filename} is valid")
         return 0
     else:
-        print(f'{filename} is invalid (server: {server}):', file=sys.stderr)
-        print('\n'.join(response['errors']), file=sys.stderr)
+        print(f"{filename} is invalid (server: {server}):", file=sys.stderr)
+        print("\n".join(response["errors"]), file=sys.stderr)
         return 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
